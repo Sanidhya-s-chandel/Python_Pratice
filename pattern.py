aa6cea1415bf3f22ea29bdb6bb1cec67c8e4790b
Sanidhya Singh chandel <125731658+Sanidhya-s-chandel@users.noreply.github.com>
2024-12-09 22:04:30 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,16 @@
+# for i in range(1,6):
+#     print("*"*i)
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*",end = " ")
+#     print(" ")
+
+# alph = 65
+# for i in range(1,6):
+#     for j in range(i):
+#         print(f"{chr(alph)}", end ="")
+#         alph+=1
+#     print(" ")
+#     alph = 65
+
